1e7ae52b093c27e72d96a7f1bedbd6a00825dddb
Johnny Glynn <glynnjw@gmail.com>
2014-03-27 17:38:04 -0400

    GUI now creates a gui. It has basic event handling: clicking the menu item "Add" under "File" will print (on system.out) "hi".
    VideoEncoder currently just calls gui.

--- ec504project/src/Video9/GUI.java
+++ ec504project/src/Video9/GUI.java
@@ -1,7 +1,50 @@
 package Video9;
 
-public class GUI {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class GUI extends JFrame implements ActionListener {
 	
+	private JMenuBar menuBar;
+	private JMenu fileMenu;
+	private JMenuItem addMenuItem;
+	private JLabel label;
 	
+	public GUI() {
+		//Create and set up the window.
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // size of window
+        setSize(400, 500);
+        
+        // menu bar
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        addMenuItem = new JMenuItem("Add");
+        addMenuItem.addActionListener(this);
+        
+        fileMenu.add(addMenuItem);
+        
+        menuBar.add(fileMenu);
+        
+        setJMenuBar(menuBar);
+ 
+        //Display the window.
+        setVisible(true);
+	}
+	
+	public void actionPerformed (ActionEvent e) {
+		Object source = e.getSource();
+		if (source == addMenuItem) {
+			handleAddImageEvent();
+		}
+	}
+	
+	protected void handleAddImageEvent() {
+		System.out.println("hi");
+	}
 
 }
--- ec504project/src/Video9/VideoEncoder.java
+++ ec504project/src/Video9/VideoEncoder.java
@@ -2,9 +2,12 @@ package Video9;
 
 public class VideoEncoder {
 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// launch GUI
+		GUI gui = new GUI();
+		
 	}
 
 }
