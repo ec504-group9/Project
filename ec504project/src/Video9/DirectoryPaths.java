aca24dc3248967e69518f498f9d59a08f6412307
osarenk1 <osarenk1@bu.edu>
2014-04-09 14:47:35 -0400

    added DirectoryPaths.java

---
+++ ec504project/src/Video9/DirectoryPaths.java
@@ -0,0 +1,16 @@
+package Video9;
+
+public class DirectoryPaths {
+	
+	final private static String pathToImages = "/home/osarenk1/Desktop/images";
+	final private static String pathToSerializable = "/home/osarenk1/Desktop/compressed.ser";
+	
+	public static String getPathtoimages() {
+		return pathToImages;
+	}
+	public static String getPathtoserializable() {
+		return pathToSerializable;
+	}
+	
+	
+}
