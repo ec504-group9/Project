d85eccd93951916cdc77f8b5ca18430c2ea823f9
onur <onur@onur-Vostro-3300>
2014-04-23 00:50:30 -0400

    ICBI algorithm added

--- ec504project/src/Video9/Main.java
+++ ec504project/src/Video9/Main.java
@@ -28,6 +28,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		//Encoder e = new Encoder("/home/osarenk1/Desktop/images", "compressed.ser", 2);
+		//Encoder e = new Encoder("/home/onur/git/Images", "compressed.ser", 2);
 		Decoder d = new Decoder("compressed.ser");
 		/*
 		BufferedImage test = null;
--- ec504project/src/scalingAlgorithms/Upsampler.java
+++ ec504project/src/scalingAlgorithms/Upsampler.java
@@ -85,7 +85,7 @@ public class Upsampler {
 					red = ((a>>16)&0xff)*(1-x_diff)*(1-y_diff) + ((b>>16)&0xff)*(x_diff)*(1-y_diff) +
 							((c>>16)&0xff)*(y_diff)*(1-x_diff)   + ((d>>16)&0xff)*(x_diff*y_diff);
 
-					int tempRGB = 0x00000000 | // hardcode alpha
+					int tempRGB = 0xFF000000 | // hardcode alpha
 							((((int)red)<<16)&0xff0000) |
 							((((int)green)<<8)&0xff00) |
 							((int)blue) ;
@@ -98,47 +98,174 @@ public class Upsampler {
 		return upsampledFrames;
 	}
 
-
 	public List<BufferedImage> ICBIUpsample(List<BufferedImage> frames, int w, int h, int scalingFactor) {
 		List<BufferedImage> upsampledFrames = new ArrayList<BufferedImage>();
 
 		int w2 = w*2;
 		int h2 = h*2;
-
+		int I1, I2, I3, I4, I5, I6, I7, I8, I9, I30, I31, I32;
+		// For each frame 
 		for(int f=0;f<frames.size(); f++){
+
 			BufferedImage original = frames.get(f);
 			BufferedImage upSampled = new BufferedImage(w2, h2, BufferedImage.TYPE_INT_RGB);
 
 			//fill new image with pixels from old image
 			for (int i=0;i<h;i++) {
 				for (int j=0;j<w;j++) {
-					upSampled.setRGB(2*j, 2*i, original.getRGB(j, i));					
+					upSampled.setRGB(2*j, 2*i, original.getRGB(j, i));		
+					/*upSampled.setRGB(2*j, 2*i+1, 0xFF0000);
+					upSampled.setRGB(2*j+1, 2*i, 0xFF0000);
+					upSampled.setRGB(2*j+1, 2*i+1, 0xFF0000);
+					 */
 				}
 			}
 
-			//TODO
-			//get the corner cases, ie the sides
-						
+			// First Pass - For Odd Indexed Pixels
 			for (int j=3;j<h2-3;j+=2) {
 				for (int i=3;i<w2-3;i+=2) {
-					int I11 = upSampled.getRGB(i-3,j+1) + upSampled.getRGB(i-1,j-1) + upSampled.getRGB(i+1,j-3)
-							- 3*upSampled.getRGB(i-1,j+1) - 3*upSampled.getRGB(i+1,j-1)
-							+ upSampled.getRGB(i-1,j+3) + upSampled.getRGB(i+1,j+1) + upSampled.getRGB(i+3,j-1); 
 
-					int I22 = upSampled.getRGB(i-1,j-3) + upSampled.getRGB(i+1,j-1) + upSampled.getRGB(i+3,j+1) 
-							- 3*upSampled.getRGB(i-1,j-1) - 3*upSampled.getRGB(i+1,j+1) + upSampled.getRGB(i-3,j-1) 
-							+ upSampled.getRGB(i-1,j+1) + upSampled.getRGB(i+1,j+3); 
+					int tempPixel = 0xFF000000;
 
-					if(I11 < I22)
-						upSampled.setRGB(i, j, (upSampled.getRGB(i-1, j-1)+upSampled.getRGB(i+1, j+1))/2 );
+					// Get the neighbor pixel values for  the derivative calculation 
+					I1 = upSampled.getRGB(i-1,j-3);
+					I2 = upSampled.getRGB(i+1,j-3);
+					I3 = upSampled.getRGB(i-3,j-1);
+					I4 = upSampled.getRGB(i-1,j-1);
+					I5 = upSampled.getRGB(i+1,j-1);
+					I6 = upSampled.getRGB(i+3,j-1);
+					I7 = upSampled.getRGB(i-3,j+1);
+					I8 = upSampled.getRGB(i-1,j+1);
+					I9 = upSampled.getRGB(i+1,j+1);
+					I30 = upSampled.getRGB(i+3,j+1);
+					I31 = upSampled.getRGB(i-1,j+3);
+					I32 = upSampled.getRGB(i+1,j+3);
+
+					// Interpolate blue channel
+					int I11Blue = (I2&0xFF) + (I4&0xFF) + (I7&0xFF) 
+							+ (I6&0xFF) + (I9&0xFF) + (I31&0xFF) 
+							- 3*(I5&0xFF) -3*(I8&0xFF);
+
+					int I22Blue = (I1&0xFF) + (I5&0xFF) + (I30&0xFF) 
+							+ (I3&0xFF) + (I8&0xFF) + (I32&0xFF) 
+							- 3*(I4&0xFF) -3*(I9&0xFF);
+
+					if(I11Blue > I22Blue)
+						tempPixel = tempPixel | ((int)((I4&0xFF) + (I9&0xFF))/2);
 					else
-						upSampled.setRGB(i, j, (upSampled.getRGB(i+1, j-1)+upSampled.getRGB(i-1, j+1))/2 );						
+						tempPixel = tempPixel | ((int)((I5&0xFF) + (I8&0xFF))/2);					
+
+					// Interpolate green channel
+					int I11Green = ((I2>>8)&0xff) + ((I4>>8)&0xff) + ((I7>>8)&0xff) 
+							+ ((I6>>8)&0xff) + ((I9>>8)&0xff) + ((I31>>8)&0xff) 
+							- 3*((I5>>8)&0xff) - 3*((I8>>8)&0xff);
+
+					int I22Green = ((I1>>8)&0xff) + ((I5>>8)&0xff) + ((I30>>8)&0xff) 
+							+ ((I3>>8)&0xff) + ((I8>>8)&0xff) + ((I32>>8)&0xff) 
+							- 3*((I4>>8)&0xff) - 3*((I9>>8)&0xff);
+
+					if(I11Green > I22Green)
+						tempPixel = tempPixel | ((((int)((((I4>>8)&0xff) + ((I9>>8)&0xff))/2))<<8)&0xff00);
+					else
+						tempPixel = tempPixel | ((((int)((((I5>>8)&0xff) + ((I8>>8)&0xff))/2))<<8)&0xff00);
+
+					// Interpolate red channel
+					int I11Red = ((I2>>8)&0xff) + ((I4>>8)&0xff) + ((I7>>8)&0xff) 
+							+ ((I6>>8)&0xff) + ((I9>>8)&0xff) + ((I31>>8)&0xff) 
+							- 3*((I5>>8)&0xff) - 3*((I8>>8)&0xff);
+
+					int I22Red = ((I1>>16)&0xff) + ((I5>>16)&0xff) + ((I30>>16)&0xff) 
+							+ ((I3>>16)&0xff) + ((I8>>16)&0xff) + ((I32>>16)&0xff) 
+							- 3*((I4>>16)&0xff) - 3*((I9>>16)&0xff);
+
+					if(I11Red > I22Red)
+						tempPixel = tempPixel | ((((int)((((I4>>16)&0xff) + ((I9>>16)&0xff))/2))<<16)&0xff0000);
+					else
+						tempPixel = tempPixel | ((((int)((((I5>>16)&0xff) + ((I8>>16)&0xff))/2))<<16)&0xff0000);
+
+					// Set the interpolated pixel value
+					upSampled.setRGB(i, j, tempPixel);
 				}
 			}
+
+			// Second Pass - For Remaining Pixels
+			for (int j=3;j<h2-3;j++) {
+				for (int i=3-(j%2);i<w2-3;i+=2) {
+
+					int tempPixel = 0xFF000000;
+					
+					// Get the neighbor pixel values for  the derivative calculation 
+					I1 = upSampled.getRGB(i-1,j-2);
+					I2 = upSampled.getRGB(i+1,j-2);
+					I3 = upSampled.getRGB(i-2,j-1);
+					I4 = upSampled.getRGB(i  ,j-1);
+					I5 = upSampled.getRGB(i+2,j-1);
+					I6 = upSampled.getRGB(i-1,j);
+					I7 = upSampled.getRGB(i+1,j);
+					I8 = upSampled.getRGB(i-2,j+1);
+					I9 = upSampled.getRGB(i  ,j+1);
+					I30 = upSampled.getRGB(i+2,j+1);
+					I31 = upSampled.getRGB(i-1,j+2);
+					I32 = upSampled.getRGB(i+1,j+2);
+
+					// Interpolate blue channel
+					int I11Blue = (I3&0xFF) + (I4&0xFF) + (I5&0xFF) 
+							+ (I8&0xFF) + (I9&0xFF) + (I30&0xFF) 
+							- 3*(I6&0xFF) -3*(I6&0xFF);
+
+					int I22Blue = (I1&0xFF) + (I6&0xFF) + (I31&0xFF) 
+							+ (I2&0xFF) + (I7&0xFF) + (I32&0xFF) 
+							- 3*(I4&0xFF) -3*(I9&0xFF);
+
+					if(I11Blue > I22Blue)
+						tempPixel = tempPixel | ((int)((I4&0xFF) + (I9&0xFF))/2);
+					else
+						tempPixel = tempPixel | ((int)((I6&0xFF) + (I7&0xFF))/2);					
+
+					// Interpolate green channel
+					int I11Green = ((I3>>8)&0xff) + ((I4>>8)&0xff) + ((I5>>8)&0xff) 
+							+ ((I8>>8)&0xff) + ((I9>>8)&0xff) + ((I30>>8)&0xff) 
+							- 3*((I6>>8)&0xff) - 3*((I7>>8)&0xff);
+
+					int I22Green = ((I1>>8)&0xff) + ((I6>>8)&0xff) + ((I31>>8)&0xff) 
+							+ ((I2>>8)&0xff) + ((I7>>8)&0xff) + ((I32>>8)&0xff) 
+							- 3*((I4>>8)&0xff) - 3*((I9>>8)&0xff);
+
+					if(I11Green > I22Green)
+						tempPixel = tempPixel | ((((int)((((I4>>8)&0xff) + ((I9>>8)&0xff))/2))<<8)&0xff00);
+					else
+						tempPixel = tempPixel | ((((int)((((I6>>8)&0xff) + ((I7>>8)&0xff))/2))<<8)&0xff00);
+
+					// Interpolate red channel
+					int I11Red = ((I3>>8)&0xff) + ((I4>>8)&0xff) + ((I5>>8)&0xff) 
+							+ ((I8>>8)&0xff) + ((I9>>8)&0xff) + ((I30>>8)&0xff) 
+							- 3*((I6>>8)&0xff) - 3*((I7>>8)&0xff);
+
+					int I22Red = ((I1>>16)&0xff) + ((I6>>16)&0xff) + ((I31>>16)&0xff) 
+							+ ((I2>>16)&0xff) + ((I7>>16)&0xff) + ((I32>>16)&0xff) 
+							- 3*((I4>>16)&0xff) - 3*((I9>>16)&0xff);
+
+					if(I11Red > I22Red)
+						tempPixel = tempPixel | ((((int)((((I4>>16)&0xff) + ((I9>>16)&0xff))/2))<<16)&0xff0000);
+					else
+						tempPixel = tempPixel | ((((int)((((I6>>16)&0xff) + ((I7>>16)&0xff))/2))<<16)&0xff0000);
+
+					// Set the interpolated pixel value
+					upSampled.setRGB(i, j, tempPixel);
+				}
+			}
+
+			try {
+				ImageIO.write(upSampled, "png", new File("upsampledimage.png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			upsampledFrames.add(upSampled);
+			System.out.println(f);
 		}
 
-			System.out.print("Up sampled succesfully!\n");
-			return upsampledFrames;
-		}
+		System.out.print("Up sampled succesfully!\n");
+		return upsampledFrames;
 	}
+}
